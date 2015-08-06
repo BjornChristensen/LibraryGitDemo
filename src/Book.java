6a57357554c7c3f58ed15af8af4baf3529136750
BjornChristensen <bjorn4000@gmail.com>
2015-08-06 14:08:25 +0200

    Initial checkin

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+
+public class Book {
+	String title="untitled";
+	
+	void display(){
+		System.out.println("title: "+title);
+	}
+
+}
---
+++ src/LibraryGitDemo.java
@@ -0,0 +1,11 @@
+
+public class LibraryGitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		Book book1=new Book();
+		book1.display();
+		
+	}
+
+}
