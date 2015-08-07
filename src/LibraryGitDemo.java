cde0ebd5bc7c5dacbdb0f0c185ccffcce3dc5167
bjchr <bjchr@DIPL-L0268.win.dtu.dk>
2015-08-07 15:58:35 +0200

    Welcome message from Library

--- src/LibraryGitDemo.java
+++ src/LibraryGitDemo.java
@@ -2,7 +2,7 @@
 public class LibraryGitDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome to LibraryGit Demo");
 		Book book1=new Book();
 		book1.display();
 		
